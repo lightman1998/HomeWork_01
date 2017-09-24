134a1694d53108b6e371bc60e00d644c70baad97
lightman1998 <kall-lightman@yandex.ru>
2017-09-24 20:21:46 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "stack.hpp"
+using namespace std;
+
+int main() {
+	stack<int> stack_A(10);
+	size_t A = 0;
+	int element_A;
+
+	while (A++ < 10) {
+		cin >> element_A;
+		stack_A.push(element_A);
+	}
+	cout << "stack elements:" << endl;
+	cout << endl;
+	stack_A.print_stack();
+	for (int i = 10; i > 5; i--)
+		stack_A.pop();
+	cout << "after removing: " << endl;
+	stack_A.print_stack();
+	system("pause");
+	return 0;
+}
