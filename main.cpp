32b8da7eca622b6b591f4849693243811e2eff98
lightman1998 <kall-lightman@yandex.ru>
2017-10-02 00:47:24 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,24 @@
 using namespace std;
 
 int main() {
-	stack<int> stack_A(10);
-	size_t A = 0;
-	int element_A;
+	try {
+		stack<int> stack_A(10);
+		size_t A = 0;
+		int element_A;
 
-	while (A++ < 10) {
-		cin >> element_A;
-		stack_A.push(element_A);
+		while (A++ < 10) {
+			cin >> element_A;
+			stack_A.push(element_A);
+		}
+		cout << "stack elements:" << endl;
+		cout << endl;
+		stack_A.print_stack();
+		for (int i = 10; i > 5; i--)
+			stack_A.pop();
+		cout << "after removing: " << endl;
+		stack_A.print_stack();
+		system("pause");
+		return 0;
 	}
-	cout << "stack elements:" << endl;
-	cout << endl;
-	stack_A.print_stack();
-	for (int i = 10; i > 5; i--)
-		stack_A.pop();
-	cout << "after removing: " << endl;
-	stack_A.print_stack();
-	system("pause");
-	return 0;
+	catch(...){}
 }
